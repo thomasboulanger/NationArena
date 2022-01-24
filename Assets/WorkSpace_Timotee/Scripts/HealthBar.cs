02f72afbc8c09f9ea33ad7e8b9515dd9f36315ea
TimoteeBaboulin <98083221+TimoteeBaboulin@users.noreply.github.com>
2022-01-24 15:10:01 +0100

    Updated Health script and Test Scene

--- Assets/WorkSpace_Timotee/Scripts/HealthBar.cs
+++ Assets/WorkSpace_Timotee/Scripts/HealthBar.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Health health;
+    private Health _health;
     public GameObject healthBar;
 
     private void OnTest()
@@ -18,45 +18,41 @@ public class HealthBar : MonoBehaviour
     void Start()
     {
         //Cree une barre de vie de 100 pvs
-        health = new Health(100);
+        _health = new Health(100);
         //Initialize le compteur de texte
-        healthBar.GetComponentInChildren<Text>().text = health.GetLife().ToString();
-    }
-
-    void Update()
-    {
-        
+        healthBar.GetComponentInChildren<Text>().text = _health.GetLife().ToString();
     }
 
     public void GetHit(int damage)
     {
-        health.GetHit(damage);
+        _health.GetHit(damage);
         //Update le nombre de point de vie
-        healthBar.GetComponentInChildren<Text>().text = health.GetLife().ToString();
+        healthBar.GetComponentInChildren<Text>().text = _health.GetLife().ToString();
         //Recupere la liste des images
         Component[] imageList = healthBar.GetComponentsInChildren<Image>();
         //Pour chaque component:
         foreach (Component comp in imageList)
         {
             //Verifie quelle partie de la barre de vie ils sont
+            //Note: This is only important if the healthbar is composed of 3 different sprites, otherwise it call all be deprecated
             switch (comp.gameObject.name)
             {
                 //Si c'est la droite, prend en compte seulement les 12 premiers pv
                 case "BarRight":
-                    (comp as Image).fillAmount = (float) (health.GetLife() - 88f) / 12;
+                    (comp as Image).fillAmount = (float) (_health.GetLife() - 88f) / 12;
                     break;
                 //Si c'est le milieu, prend en compte seulement les 76 pv du milieu
                 case "Bar":
-                    (comp as Image).fillAmount = (float) (health.GetLife() - 12f) / 76;
+                    (comp as Image).fillAmount = (float) (_health.GetLife() - 12f) / 76;
                     break;
                 //Si c'est la gauche, prend en compte seulement les 12 derniers pv
                 case "BarLeft":
-                    (comp as Image).fillAmount = (float) health.GetLife() / 12;
+                    (comp as Image).fillAmount = (float) _health.GetLife() / 12;
                     break;
             }
         }
         //Si le personnage atteint 0 pv, detruit le player
-        if (health.IsDead())
+        if (_health.IsDead())
             Destroy(gameObject);
     }
 }
