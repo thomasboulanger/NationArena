f3252786c8f2d2dcad73524bf8fda46b8b689b87
VIKAIN <thomas.hebert84@orange.fr>
2022-01-24 16:43:16 +0100

    Working on random events

---
+++ Assets/WorkSpace_Thomas/Scripts/LightsOffPresets.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LightsOffPreset", menuName = "ScriptableObjects/LightOffPreset", order = 1)]
+public class LightsOffPresets : ScriptableObject
+{
+    [Header("Conditons")] 
+    public float EventMinDuration;
+    public float EventMaxDuration;
+    public int MaxPlayerAliveToLaunch;
+    
+    [Header("Fog")]
+    public Color FogColor;
+    public float FogDensity;
+    [Range(0,1)]public float HaloStrength;
+
+    [Header("Lights")]
+    public Color LightColor;
+    public float Intensity;
+}
