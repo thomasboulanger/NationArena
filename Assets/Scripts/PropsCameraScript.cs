3ae3e038e1b91388791cb438f497e54274955e6a
TimoteeBaboulin <98083221+TimoteeBaboulin@users.noreply.github.com>
2022-01-26 11:08:00 +0100

    Added Camera Scripts

--- Assets/Scripts/PropsCameraScript.cs
+++ Assets/Scripts/PropsCameraScript.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PropsCameraScript : MonoBehaviour
 {
+    private GameObject _target;
+
+    public float distance;
+    public float rotateSpeed;
+    public float baseAngle;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        baseAngle = baseAngle / 180;
+        _target = GameObject.FindWithTag("Arena");
+        transform.position =
+            new Vector3((float) Math.Sin(baseAngle * Math.PI) * distance, 5, (float) Math.Cos(baseAngle * Math.PI) * distance);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.RotateAround(_target.transform.position, Vector3.up, (rotateSpeed * Time.deltaTime));
+        transform.LookAt(_target.transform);
+        transform.Rotate(Vector3.up, 90);
     }
 }
